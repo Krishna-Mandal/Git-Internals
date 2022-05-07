7c767170b5aa418135ef872170ccd04ca772e46f
Krishna Kumar Mandal <krishiway@gmail.com>
2022-05-07 21:24:27 +0200

    Stage 1 Completed

--- Git Internals/task/src/gitinternals/Main.kt
+++ Git Internals/task/src/gitinternals/Main.kt
@@ -1,5 +1,32 @@
 package gitinternals
 
+import java.io.File
+import java.io.FileInputStream
+import java.util.zip.InflaterInputStream
+
 fun main() {
-    // write your code here
+    println("Enter git object location:")
+    val gitObjectsPath = readln()
+    var gitObjectFile = GitObjectInflater(gitObjectsPath)
+    gitObjectFile.inflateBlob()
+}
+
+class GitObjectInflater(var fileName: String) {
+
+    fun inflateBlob() {
+        val file = File(this.fileName)
+        val fis = FileInputStream(file)
+        val iis = InflaterInputStream(fis)
+
+        while (iis.available() == 1) {
+            for(char in String(iis.readAllBytes())) {
+                if(char.code == 0) {
+                    println()
+                } else {
+                    print(char)
+                }
+            }
+        }
+    }
+
 }
